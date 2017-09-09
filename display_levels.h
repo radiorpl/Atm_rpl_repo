7a2740e989b277460e7b91af9d398862286990e8
Richard Levinson <radiorpl@users.noreply.github.com>
2017-09-08 23:30:38 -0400

    rename delay mix level

--- display_levels.h
+++ display_levels.h
@@ -5,9 +5,10 @@ extern int mas_vol_level;
 extern int vol_wav_1_level; 
 extern int vol_wav_2_level; 
 //delay
+extern int on_off;
 extern int send_1_level;
 extern int send_2_level;
-extern int mix_level;
+extern int delay_mix_level;
 extern int time_1_level;
 extern int time_2_level;
 extern int time_3_level;
